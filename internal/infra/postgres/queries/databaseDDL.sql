5e66d9e73cb1e1a6b87795def0c3053e03da9510
João Victor da Silva <jv782063@gmail.com>
2023-10-31 10:35:08 -0300

    update ddl for database

--- internal/infra/postgres/queries/databaseDDL.sql
+++ internal/infra/postgres/queries/databaseDDL.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE "medicalapi";
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS "patients" (
-    "id" UUID NOT NULL PRIMARY KEY,
-    "name" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "document" VARCHAR(11) NOT NULL,
-    "birth_date" DATE NOT NULL,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL
+  "id" UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+  "name" VARCHAR(255) NOT NULL,
+  "email" VARCHAR(255) NOT NULL UNIQUE,
+  "document" VARCHAR(11) NOT NULL,
+  "birth_date" DATE NOT NULL,
+  "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+  "updated_at" TIMESTAMP NOT NULL DEFAULT now()
 );
 
