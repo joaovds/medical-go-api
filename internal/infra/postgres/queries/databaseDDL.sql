0eb1b3612d5f1286ed00f59710c2909d57e2961c
João Victor da Silva <jv782063@gmail.com>
2023-11-02 10:11:00 -0300

    doctors ddl

--- internal/infra/postgres/queries/databaseDDL.sql
+++ internal/infra/postgres/queries/databaseDDL.sql
@@ -12,3 +12,13 @@ CREATE TABLE IF NOT EXISTS "patients" (
   "updated_at" TIMESTAMP NOT NULL DEFAULT now()
 );
 
+CREATE TABLE IF NOT EXISTS "doctors" (
+	"id" UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+	"name" VARCHAR(255) NOT NULL,
+	"email" VARCHAR(255) NOT NULL UNIQUE,
+	"password" VARCHAR(255) NOT NULL,
+	"document" VARCHAR(11) NOT NULL,
+	"created_at" TIMESTAMP NOT NULL DEFAULT now(),
+	"updated_at" TIMESTAMP NOT NULL DEFAULT now()
+)
+
